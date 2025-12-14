551d2d19d5fa91bde948a0f0fda51727d52951ce
Damien Ronssin <damien.ronssin@gmail.com>
2025-12-14 12:37:40 +0100

    Show some more info on UI

--- DummyCachedComponent.h
+++ DummyCachedComponent.h
@@ -7,7 +7,8 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-class DummyCachedComponent : public juce::Component
+class DummyCachedComponent : public juce::Component,
+                             juce::Timer
 {
 public:
 
@@ -15,6 +16,17 @@ public:
     {
         mBufferedImage = shouldBufferToImageOnStart;
         setBufferedToImage(mBufferedImage);
+        startTimerHz(30);
+    }
+
+    void timerCallback() override
+    {
+        bool using_cached_real = getCachedComponentImage() != nullptr;
+
+        if (using_cached_real != mUsingBufferedImageReal) {
+            mUsingBufferedImageReal = using_cached_real;
+            repaint();
+        }
     }
 
     void resized() override {}
@@ -35,11 +47,27 @@ public:
         g.setColour(juce::Colours::white);
         g.drawRect(getLocalBounds().reduced(1), 1);
         g.drawText("Some text that can be bufferedToImage", getLocalBounds(), juce::Justification::centred, false);
+
+        if (mUsingBufferedImageReal) {
+            g.setColour(juce::Colours::green);
+            g.drawText("Using Buffered Image real",
+                       getLocalBounds().withTop(0).withHeight(20),
+                       juce::Justification::topLeft,
+                       false);
+        } else {
+            g.setColour(juce::Colours::red);
+            g.drawText("Not Using Buffered Image real",
+                       getLocalBounds().withTop(0).withHeight(20),
+                       juce::Justification::topLeft,
+                       false);
+        }
     }
 
 private:
 
     bool mBufferedImage = false;
+
+    bool mUsingBufferedImageReal = false;
 };
 
 #endif //SIMPLEEFFECT_DUMMYCACHEDCOMPONENT_H
